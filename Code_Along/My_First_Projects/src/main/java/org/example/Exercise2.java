4b049da36d3a9cf2de2b06f496310f69efb5f985
oTheng <124187649+oTheng@users.noreply.github.com>
2026-04-08 13:48:08 -0400

    initial
    main branch

---
+++ Code_Along/My_First_Projects/src/main/java/org/example/Exercise2.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Code_Along/My_First_Projects/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+    float Pi = 3.4f;
+    boolean isMember = true;
+    double PI = 3.412314;
+    int age = 65;
+    long  Stars = 1231243414;
+    char letterGrade = 'A';
+    letterGrade = '3';
+    Pi = 2313.23f;
+
+
+
+    }
+}
