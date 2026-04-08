f95eb0205b0bc9d5ac7ef3f1f45ce07d0fd1bd8b
Tsiu11 <Tsiu11@wgu.edu>
2026-04-08 13:54:03 -0400

    Added UserInput.java

---
+++ Code_Along/My_First_Projects/src/main/java/org/example/UserInput.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class UserInput {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(InputStream.nullInputStream());
+
+
+
+    }
+}
